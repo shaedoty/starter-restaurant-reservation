d0a5805079196716016c801159826535d08039a3
Shae Doty <113932523+shaedoty@users.noreply.github.com>
2023-02-16 19:56:43 -0500

    Update ReservationsList.js

--- front-end/src/reservations/ReservationsList.js
+++ front-end/src/reservations/ReservationsList.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReservationCard from "./ReservationCard";
 
-// Defines the reservations list for the dashboard and search pages.
-// If 'searchMode' is true, all reservations are returned for the search page.
-// If 'searchMode' is false, reservations with a status of 'cancelled' are not returned for the dashboard.
+
 
 export default function ReservationsList({ reservations, searchMode }) {
   if (searchMode) {
