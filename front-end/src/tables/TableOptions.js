2f177098354015f4dc16f6f6d10d9fc73514ec7b
Shae Doty <113932523+shaedoty@users.noreply.github.com>
2023-02-16 20:00:11 -0500

    Update TableOptions.js

--- front-end/src/tables/TableOptions.js
+++ front-end/src/tables/TableOptions.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-// Defines the table options for use in the SeatReservations component.
-
 export default function TableOptions({ table }) {
   return (
     <option value={table.table_id}>
